9c3502f9e8326e1e00b1e0ff04c0c7baf4eb8ff7
Vityy <ygensoftvd@gmail.com>
2025-10-23 12:25:46 +0200

    TP02 Create Table fait

--- creation_compta1.sql
+++ creation_compta1.sql
@@ -1,27 +1,27 @@
 CREATE TABLE article(
-	id INT PRIMARY KEY,
-    ref INT,
-    designation TEXT,
+	id INT(11) AUTO_INCREMENT PRIMARY KEY,
+    ref INT(11),
+    designation VARCHAR(50),
     prix DECIMAL,
-    id_fou INT
+    id_fou INT(11)
 );
 
 CREATE TABLE fournisseur(
-    id INT PRIMARY KEY,
-    nom TEXT
+    id INT(11) AUTO_INCREMENT PRIMARY KEY,
+    nom VARCHAR(50)
 );
 
 CREATE TABLE bon(
-	id INT PRIMARY KEY,
-    numero INT,
+	id INT(11) AUTO_INCREMENT PRIMARY KEY,
+    numero INT(15),
     date_cmde DATE,
-    delai DECIMAL,
-    id_fou INT
+    delai DATE,
+    id_fou INT(11)
 );
 
 CREATE TABLE compo(
-	id INT PRIMARY KEY,
-    qte INT,
-    id_art INT,
-    id_bon INT
+	id INT(11) AUTO_INCREMENT PRIMARY KEY,
+    qte INT(2),
+    id_art INT(11),
+    id_bon INT(11)
 );
